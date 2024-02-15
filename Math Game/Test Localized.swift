9f4766ff8f2b6ee719326cac04fc150403c0e69a
colefortner <colefortner@gmail.com>
2024-02-15 16:40:51 -0600

    Add some localization options for the app

---
+++ Math Game/Test Localized.swift
@@ -0,0 +1,28 @@
+//
+//  Test Localized.swift
+//  Math Game
+//
+//  Created by Cole Fortner on 2/15/24.
+//
+
+import SwiftUI
+
+struct Test_Localized: View {
+    var body: some View {
+        VStack {
+            Text("Game Over")
+            Text("Score")
+            Text("Level")
+            Text("Game")
+            Text("High Scores")
+            Text("Rank")
+            Text("Name")
+            Text("Save")
+        }.font(.title)
+        
+    }
+}
+
+#Preview {
+    Test_Localized()
+}
