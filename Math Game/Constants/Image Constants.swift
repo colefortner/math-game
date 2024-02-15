17df634c774e72b1ee26bd72b335978324ca8d18
colefortner <colefortner@gmail.com>
2024-02-15 16:06:24 -0600

    Import creature image assets to Creature folder in Assets.  Create Creatures enum in Image Constants file

---
+++ Math Game/Constants/Image Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Image Constants.swift
+//  Math Game
+//
+//  Created by Cole Fortner on 2/15/24.
+//
+
+import Foundation
+
+enum Creatures: String {
+    case creature0, creature1, creature2, creature3, creature4, creature5
+}
